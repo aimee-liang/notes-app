7a50d771ef46df1c158da403581fd0e4744dce22
aimee-liang <aimeeeliang@gmail.com>
2021-03-11 18:53:04 -0500

    Testing to see if I can push this to GitHub

---
+++ Notes/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Aimee Liang on 3/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet var table: UITableView!
+    @IBOutlet var label: UILabel!
+    
+    var models: [(title: String, note: String)] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        table.delegate = self
+        table.dataSource = self
+        title = "Notes"
+    }
+    @IBAction func didTapNewNote(){
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSections section: Int){
+        return models.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: IndexPath)
+        cell.textLabel?.text = models[indexPath.row].title
+        cell.detailTextLabel?.text = models[indexPath.row].note
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: <#T##IndexPath#>, animated: true)
+        
+        guard let vc = storyboard?.instantiateViewController(identifier: "note") as? NoteViewController else {
+            return
+        }
+        vc.title = "Note"
+        UINavigationController.pushViewController(vc, ani)
+        }
+    }
+    
+}
