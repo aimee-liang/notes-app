35f8e4ab8d48aa893a879f46f0cefbfb764c4b3b
aimee-liang <aimeeeliang@gmail.com>
2021-03-11 17:18:46 -0500

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Aimee Liang on 3/11/21.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
