7959974099ab6561ebc80f812dfe2543ed855277
aimee-liang <aimeeeliang@gmail.com>
2021-03-14 22:57:20 -0400

    AppDelegate - didDiscardSceneSessions() functionality

--- Notes/AppDelegate.swift
+++ Notes/AppDelegate.swift
@@ -18,4 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate{
     func application(_application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration{
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
+    
+    func application(_application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>){
+        
+    }
 }
